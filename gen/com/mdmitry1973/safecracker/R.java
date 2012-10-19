d6ad22af56d8cef57930c881061f998945de584d
mdmitry1973 <mdmitry1973@gmail.com>
2012-10-18 19:54:22 -0600

    remove unnecessary files

--- gen/com/mdmitry1973/safecracker/R.java
+++ gen/com/mdmitry1973/safecracker/R.java
@@ -14,22 +14,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int coin_gold=0x7f020000;
-        public static final int coin_gold_1=0x7f020001;
-        public static final int coin_gold_2=0x7f020002;
-        public static final int coin_gold_3=0x7f020003;
-        public static final int coin_gold_4=0x7f020004;
-        public static final int coin_gold_org=0x7f020005;
-        public static final int coin_silver=0x7f020006;
-        public static final int coin_silver_1=0x7f020007;
-        public static final int coin_silver_2=0x7f020008;
-        public static final int coin_silver_3=0x7f020009;
-        public static final int coin_silver_4=0x7f02000a;
-        public static final int coin_silver_org=0x7f02000b;
-        public static final int ic_action_search=0x7f02000c;
-        public static final int ic_item_number_background=0x7f02000d;
-        public static final int ic_launcher=0x7f02000e;
-        public static final int safecracking=0x7f02000f;
+        public static final int coin_gold_1=0x7f020000;
+        public static final int coin_gold_2=0x7f020001;
+        public static final int coin_gold_3=0x7f020002;
+        public static final int coin_gold_4=0x7f020003;
+        public static final int coin_silver_1=0x7f020004;
+        public static final int coin_silver_2=0x7f020005;
+        public static final int coin_silver_3=0x7f020006;
+        public static final int coin_silver_4=0x7f020007;
+        public static final int ic_action_search=0x7f020008;
+        public static final int ic_item_number_background=0x7f020009;
+        public static final int safecracking=0x7f02000a;
     }
     public static final class id {
         public static final int ListView1=0x7f070001;
